2beb1e31e9dcbcf8a8d30cf71daf768855178b76
Lucie <82907637+lynxur@users.noreply.github.com>
2021-09-11 22:24:42 +0200

    template erstellung mit jinja

--- generate.py
+++ generate.py
@@ -1,11 +1,20 @@
+from jinja2 import Template
 
-with open("kalender.html", "r") as F:
-    htmltext = F .read()
 
-htmltext = htmltext.replace("[[MONAT]]", "JANUAR")
+with open("template.html", "r") as F:
+    templatetext = F .read()
 
-with open("kalender_1.html", "w") as F:
-    F.write(htmltext)
+tempi = Template(templatetext)
+
+outstring = tempi.render(wochen = [
+    {"monat": "Januar", 
+    "tage": [    
+        {"name": "Montag", "table-ID": 1, "datum": "06.09."},
+        {"name": "Dienstag", "table-ID": 2, "datum": "07.09."},
+        {"name": "Mittwoch", "table-ID": 1, "datum": "08.09."}]}])
+
+with open("renderedhtml.html", "w") as F:
+    F.write(outstring)
 
 
 
