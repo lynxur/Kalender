9cfd658ea466dc3a3e6c7ac60d7eab02729b4189
Lucie <82907637+lynxur@users.noreply.github.com>
2021-09-13 14:45:39 +0200

    Render working kalender

--- generate.py
+++ generate.py
@@ -1,23 +1,59 @@
+import locale
+from datetime import date, time, timedelta
 from jinja2 import Template
 
+# Setze Region für Sprache
+locale.setlocale(locale.LC_TIME, "de_DE")
 
 with open("template.html", "r") as F:
     templatetext = F .read()
 
 tempi = Template(templatetext)
 
+year = 2022
 
-outstring = tempi.render(seiten = [
-    {"monat": "Januar", 
-    "tage": [    
-        {"name": "Montag", "table-ID": 1, "datum": "06.09."},
-        {"name": "Dienstag", "table-ID": 2, "datum": "07.09."},
-        {"name": "Mittwoch", "table-ID": 1, "datum": "08.09."}]},
-    {"monat": "Januar", 
-    "tage": [    
-        {"name": "Montag", "table-ID": 1, "datum": "06.09."},
-        {"name": "Dienstag", "table-ID": 2, "datum": "07.09."},
-        {"name": "Mittwoch", "table-ID": 1, "datum": "08.09."}]}])
+start_date = date(year, 1, 1)
+
+real_start_date = start_date - timedelta(days=start_date.weekday())
+current_date = real_start_date
+
+end_date = date(year, 12, 31)
+
+real_end_date = end_date + timedelta(days=(6 - end_date.weekday()))
+
+dates = []
+
+while current_date <= real_end_date:
+    dates.append(current_date)
+    current_date += timedelta(days=1)    
+
+anz_wochenseiten = len(dates) // 7
+
+seiten = []
+for wdx in range(anz_wochenseiten):
+    seite = {
+        "monat": dates[wdx * 7 + 6].strftime("%B"),
+        "tage": [],
+        }
+    for tdx in range(7):
+        day = dates[(wdx * 7) + tdx]
+        seite['tage'].append(
+            {
+                "name": day.strftime("%A"),
+                "table-ID": tdx % 2 + 1,
+                "datum": day.strftime("%d.%m."),
+            }
+        )
+    seite['tage'].append(
+            {
+                "name": "Notizen",
+                "table-ID": 2,
+                "datum": "",
+            }
+        )
+    seiten.append(seite)  
+
+outstring = tempi.render(seiten = seiten)
 
 with open("renderedhtml.html", "w") as F:
     F.write(outstring)
