0f299bf208f275a8247753c4e39d9ea1597c5d51
Florian <florian@flova.de>
2021-09-20 19:05:04 +0200

    Big refactor of the python scipt

--- generate.py
+++ generate.py
@@ -1,9 +1,61 @@
 import locale
 import webbrowser
+from typing import Dict, Iterable
 from datetime import date, time, timedelta
+from dateutil.relativedelta import relativedelta
 from jinja2 import Template
 
-# Setze Region für Sprache
+def get_next_sunday_date(input_date: date) -> date:
+    return input_date + timedelta(days=(6 - input_date.weekday()))
+
+def get_previous_monday_date(input_date: date) -> date:
+    return input_date - timedelta(days=input_date.weekday())
+
+def build_wochen_seite(input_date: date) -> Dict:
+    # Create page dictionary
+    seite = {
+        "monat": get_next_sunday_date(input_date).strftime("%B"),
+        "type": "Woche",
+        "tage": [],
+        }
+    # Iterate over all days in the week
+    for i, day in enumerate(date_iterator(input_date, timedelta(days=1), 7)):
+        # Create a nested dict for each day
+        seite['tage'].append(
+            {
+                "name": day.strftime("%A"),
+                "table-ID": i % 2 + 1,
+                "datum": day.strftime("%d.%m."),
+            }
+        )
+    # Append blank "day" for notes to get to an even number on the page
+    seite['tage'].append({"name": "Notizen", "table-ID": 2, "datum": ""})
+    return seite
+
+def build_monats_ueberblick(input_date: date) -> Dict:
+    wochen = []
+
+    current_date = get_previous_monday_date(input_date)
+    real_end_date = get_next_sunday_date(input_date + relativedelta(months=1))
+
+    while current_date <= real_end_date:
+        tage = []
+        for j in range(7):
+            tage.append(current_date.strftime("%d"))
+            current_date += timedelta(days=1)
+        wochen.append(tage)
+
+    return {
+            "monat": get_next_sunday_date(input_date).strftime("%B"),
+            "type": "Uebersicht",
+            "wochen": wochen,
+        }
+
+def date_iterator(start_date: date, step: timedelta, num:int) -> Iterable[date]:
+    return (start_date + n * step for n in range(num))
+
+
+# Set region for corret date formating depending on the region
 locale.setlocale(locale.LC_TIME, "de_DE")
 
 with open("template.html", "rb") as F:
@@ -14,68 +66,20 @@ tempi = Template(templatetext)
 year = 2022
 
 start_date = date(year, 1, 1)
-
-real_start_date = start_date - timedelta(days=start_date.weekday())
-current_date = real_start_date
-
 end_date = date(year, 12, 31)
 
-real_end_date = end_date + timedelta(days=(6 - end_date.weekday()))
-
-dates = []
-
-while current_date <= real_end_date:
-    dates.append(current_date)
-    current_date += timedelta(days=1)    
-
-anz_wochenseiten = len(dates) // 7
+real_end_date = get_next_sunday_date(end_date)
+current_date = get_previous_monday_date(start_date)
 
 seiten = []
-for wdx in range(anz_wochenseiten):
-    
+
+while current_date <= real_end_date:
     # Monatsübersicht
-    if len(seiten) == 0 or \
-            seiten[-1]["monat"] != dates[wdx * 7 + 6].strftime("%B"):
-        
-        wochen = []
-        for i in range(4):
-            tage = []
-            for j in range(7):
-                tage.append((dates[wdx * 7]+timedelta(days = i * 7 + j)).strftime("%d"))
-            wochen.append(tage)
-
-
-        seiten.append(
-            {
-                "monat": dates[wdx * 7 + 6].strftime("%B"),
-                "type": "Uebersicht",
-                "wochen": wochen,
-            }
-        )
-
+    if len(seiten) == 0 or seiten[-1]["monat"] != get_next_sunday_date(current_date).strftime("%B"):
+       seiten.append(build_monats_ueberblick(current_date)) 
     #Wochenseite
-    seite = {
-        "monat": dates[wdx * 7 + 6].strftime("%B"),
-        "type": "Woche",
-        "tage": [],
-        }
-    for tdx in range(7):
-        day = dates[(wdx * 7) + tdx]
-        seite['tage'].append(
-            {
-                "name": day.strftime("%A"),
-                "table-ID": tdx % 2 + 1,
-                "datum": day.strftime("%d.%m."),
-            }
-        )
-    seite['tage'].append(
-            {
-                "name": "Notizen",
-                "table-ID": 2,
-                "datum": "",
-            }
-        )
-    seiten.append(seite)  
+    seiten.append(build_wochen_seite(current_date))  
+    current_date += timedelta(days=7)
 
 outstring = tempi.render(seiten = seiten)
 
